ca1e18704243ff906f9500d7b350ff591983833c
Gabriel Ivasuc <gabrielivasuc@yahoo.com>
2024-11-05 12:07:33 +0200

    created button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import ProjectSideBar from "./components/ProjectSideBar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
   return (
     <main className="flex h-screen gap-10 my-8">
       <ProjectSideBar/>
-      <NewProject/>
+      <NoProjectSelected/>
     </main>
   );
 }
--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -1,9 +1,9 @@
-export default function() {
+import Button from './Button.jsx'
+
+export default function ProjectSideBar() {
   return <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-xl">
     <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-300">Your Projects</h2>
-    <div>
-      <button className="px-4 py-2 text-xs rounded-md md:text-base bg-stone-700 text-stone-400 hover:bg-stone-700 hover:text-stone-100">Add project</button>
-    </div>
+      <Button text='Add new Project' />
     <ul></ul>
   </aside>
 }
